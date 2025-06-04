a71c89c55d3322efbb3b49beb23d5ce21ca74915
Sushantmg <sushantmg2004@gmail.com>
2025-06-04 23:37:48 +0545

    authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+"use client";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/clerk-react";
+import { SignOutButton } from "@clerk/nextjs";
+import React from "react";
 
 const Homepage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <div>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
